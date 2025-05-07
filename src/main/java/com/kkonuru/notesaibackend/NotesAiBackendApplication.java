1ec2110b09f744e60a8e95a3b56c2eac6a9203a8
KKonuru <kausthubhkonuru@gmail.com>
2025-05-07 16:14:20 -0400

    Project setup

---
+++ src/main/java/com/kkonuru/notesaibackend/NotesAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kkonuru.notesaibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesAiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotesAiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kkonuru/notesaibackend/NotesAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkonuru.notesaibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesAiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
